1d98934fb8c7fe7141edf6a06362e059f24e1bd2
amirhs1002 <amirhs1002@atomicmail.io>
2026-08-22 07:12:04 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Cal
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-TELEGRAM_BOT_TOKEN = "8769828959:AAFH297QURtqXLSehxzMnup2j2ghBH1oTOk"
+TELEGRAM_BOT_TOKEN = "8769828959:AAHC2CNYcIuf21qKKaRNAZ8GTiDaNuVuSjQ"
 OWNER_USERNAME = "amirhszz"
 owner_chat_id = 6422509900
 
